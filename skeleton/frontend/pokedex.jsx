76425de476b5712e7fea280f4deef3754f3ca5be
jondominguez94 <jon.dominguez94@yahoo.com>
2018-12-06 22:02:35 -0800

    Add thunk middleware

--- skeleton/frontend/pokedex.jsx
+++ skeleton/frontend/pokedex.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 import { receiveAllPokemon } from './actions/pokemon_actions';
 import { fetchAllPokemon } from './util/api_util';
@@ -8,6 +9,10 @@ window.receiveAllPokemon = receiveAllPokemon;
 window.fetchAllPokemon = fetchAllPokemon;
 
 document.addEventListener("DOMContentLoaded", () => {
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  
   const rootEl = document.getElementById("root");
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
 });
