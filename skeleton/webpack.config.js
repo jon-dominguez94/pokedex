6416fc9f938dccb85b04fb40ee36c1b6604678a9
jondominguez94 <jon.dominguez94@yahoo.com>
2018-12-06 21:27:46 -0800

    Fix config and setup frontend structure

--- skeleton/webpack.config.js
+++ skeleton/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   context: __dirname,
   entry: "./frontend/pokedex.jsx",
   output: {
-    path: path.resolve(__dirname, 'app', 'assests', 'javascripts'),
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js"
   },
   module: {
